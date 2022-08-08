48f56166bf7adb415c06854cc3c936ec0fd57e2b
LucaZH <lucarandria1@gmail.com>
2022-08-08 08:42:40 +0300

    .

--- tools/scrapping.py
+++ tools/scrapping.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup as bs
-import requests
+# from bs4 import BeautifulSoup as bs
+# import requests
 
-url = "https://www.infosante.be/recherche?q=paludisme"
-r = requests.get(
-    url,
-    headers = {
-        'User-Agent': 'Popular browser\'s user-agent',
-    }
-)
-print(r.content)
+# url = "https://www.infosante.be/recherche?q=paludisme"
+# r = requests.get(
+#     url,
+#     headers = {
+#         'User-Agent': 'Popular browser\'s user-agent',
+#     }
+# )
+# print(r.content)
