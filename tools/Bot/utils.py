bd9f1cdbc166b3bcf308775ab267d215b407fb5a
LucaZH <lucarandria1@gmail.com>
2022-08-10 02:46:18 +0300

    fact

--- tools/Bot/utils.py
+++ tools/Bot/utils.py
@@ -111,44 +111,57 @@ def get_news():
 #         "text": linfo[i],
 #     })
 #     return listeinfo
-# userid = '5158134077575785'
-# ACCESS_TOKEN='EAAGfZAFN8lA4BAL3EHkJF7Fep3sSH7pwTCLuADvYrg64lTBRzjIEyC3XOExu5PvGAtnGrwhzLfvjdxDLdan7BKI8XcxypsoSJcEiuB4TVIstTC8NyuQUQjZBdGZBjMjQU6dSvybqcnPUPgxsZAxVjpAgK0k9ZCj7UDbsSY7oCwyyMlDQdxYAyM4dA3CDYoMsZD'
-# url = 'https://graph.facebook.com/v13.0/me/messages?access_token='+ACCESS_TOKEN
+userid = '5158134077575785'
+ACCESS_TOKEN='EAAGfZAFN8lA4BAL3EHkJF7Fep3sSH7pwTCLuADvYrg64lTBRzjIEyC3XOExu5PvGAtnGrwhzLfvjdxDLdan7BKI8XcxypsoSJcEiuB4TVIstTC8NyuQUQjZBdGZBjMjQU6dSvybqcnPUPgxsZAxVjpAgK0k9ZCj7UDbsSY7oCwyyMlDQdxYAyM4dA3CDYoMsZD'
+url = 'https://graph.facebook.com/v13.0/me/messages?access_token='+ACCESS_TOKEN
 # url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+ACCESS_TOKEN
-# def send_info_fact(dest_id):
-#     data = {
-#         "recipient": {
-#             "id": f'{dest_id}'
-#         },
-#         "messaging_type": "response",
-#         "message": {
-#             "attachment": {
-#                 "type": "template",
-#                 "payload": {
-#                     "template_type": "generic",
-#                     "elements": 
-#                         {
-#                             "title": f'title',
-#                             "image_url": f"https://cdn-icons-png.flaticon.com/512/2764/2764545.png",
-#                             "subtitle": f"test",
+def send_info_fact(dest_id):
+    tittreinfo=["De quoi s’agit-il ?","Quelle est sa fréquence ?","Comment le reconnaître ?","Comment le diagnostic est-il posé ?","Que pouvez-vous faire ?","Que peut faire votre médecin ?"]
+    info1="Le paludisme est une maladie infectieuse causée par le parasite Plasmodium. Ce parasite se transmet à l'homme par la piqûre d’un moustique bien spécifique : l’anophèle."
+    info2 ="Le paludisme est l’une des infections les plus répandues sur la planète. On compte chaque année plus de 200 millions de cas de paludisme et, selon les estimations, la maladie fait plus de 400 000 morts par an, essentiellement parmi les enfants d’Afrique."
+    info3= "La période d'incubation, c’est-à-dire l’intervalle entre la piqûre de moustique et l'apparition des premiers symptômes, varie de 10 jours à 1 mois. Il se peut donc que vous soyez rentré chez vous depuis un moment avant de tomber malade."
+    info4= "Toute personne ayant voyagé dans une région touchée par le paludisme et ayant de la fièvre est suspectée d’être infectée par le paludisme."
+    info5="La principale mesure à prendre est d’empêcher les moustiques de vous piquer. C’est après la tombée de la nuit qu’ils sévissent le plus. Portez des vêtements de couleur claire et recouvrant vos bras et vos jambes (manches longues, pantalons longs ou jupes longues)."
+    info6="Si vous vous rendez dans une région touchée par le paludisme, le médecin vous prescrira un médicament à titre préventif. Il évaluera le médicament qui vous convient le mieux. Il est donc tout à fait possible que vous ne preniez pas le même traitement que vos compagnons de voyage. À partir de 40 kg, la dose pour adultes doit être administrée. Pour un poids inférieur à 40 kg, la dose doit être calculée en fonction du poids."
+    linfo=[info1,info2,info3,info4,info5,info6]
+    listinfofact=[]
+    for i in range(6):
+        listinfofact.append({
+            "titre":tittreinfo[i],
+            "text": linfo[i],
+        })
+    # print(listinfofact)
 
-#                             "buttons": [
-#                                 {
-#                                     "type": "postback",
-#                                     "title": "Regarder",
-#                                     "payload": json.dumps({
-#                                         'watch': 'url'
-#                                     })
-#                                 }
-#                             ]
-#                         } 
-#                 },
-                
-#             },
-#         }
-#     }
-#     headers = {"Content-Type": "application/json"}
-#     r = requests.post(url, data=json.dumps(data), headers=headers)
-#     print(r.content)
+    data = {
+        "recipient": {
+            "id": f'{dest_id}'
+        },
+        "messaging_type": "response",
+        "message":{
+        "attachment":{
+        "type":"template",
+        "payload":{
+                "template_type":"generic",
+                "elements":[
+                    {
+                        "title":f"{info_fact['titre']}",
+                        "image_url":"https://cdn-icons-png.flaticon.com/512/2764/2764545.png",
+                        "subtitle":f"{info_fact['text']}",
+                        "buttons":[
+                            {
+                                "type":"postback",
+                                "title":"Lire",
+                                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                            }              
+                        ]      
+                    }for info_fact in listinfofact
+                ]
+            }
+    }
+  }
+    }
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    print(r.content)
 # send_info_fact(userid)
 # print(f"{userid} ")
