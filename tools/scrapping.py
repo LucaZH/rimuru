b330b3e3c4a23a32f844c037170eee0f14299197
LucaZH <lucarandria1@gmail.com>
2022-09-01 20:30:56 +0300

    Scrap

--- tools/scrapping.py
+++ tools/scrapping.py
@@ -3,62 +3,61 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-# path="/home/luca/Documents/Code/Selenium/chromedriver"
-# chop = webdriver.ChromeOptions()
-# s = Service(path)
-# chop.add_extension("/home/luca/Documents/Memoire - Licences/Memoire - Code/tools/I-don-t-care-about-cookies.crx")
-# driver = webdriver.Chrome(service=s,options=chop)
-# def get_result_search():
-#     r= []
-#     urlm=[]
-#     result = []
-#     driver.get("https://www.infosante.be/recherche?q=paludisme")
-#     listeInfo=driver.find_elements(By.CLASS_NAME, 'morphsearch-content')
-#     for itemInfo in listeInfo:
-#         textInfo = itemInfo.find_element(By.XPATH,'//*[@id="morphsearch"]/div/div/div/ul').text
-#     r = textInfo.split("\n")
-#     for i in range(1,len(r)+1):
-#         xpath=f'//*[@id="morphsearch"]/div/div/div/ul/li[{i}]/a'
-#         geturl = driver.find_element(By.XPATH,xpath).get_attribute('href')
-#         urlm.append(geturl)
-#     driver.close()
-#     for j in range(len(r)):
-#         info = {
-#             'titre' : r[j],
-#             'url': urlm[j]
-#         }
-#         result.append(info)
-#     return result
-# # //*[@id="main"]/div/div/div[1]/div[3]/p[1]/strong
-# # //*[@id="main"]/div/div/div[1]/div[3]/p[5]/strong
-# # //*[@id="main"]/div/div/div[1]/div[3]/p
-# def re():
-#     driver.get("file:///home/luca/Documents/Code/Selenium/Malaria%20(paludisme)%20%C2%B7%20Info%20sant%C3%A9.html")
-#     ggo=driver.find_elements(By.XPATH, '//*[@id="main"]/div/div/div[1]/div[3]')
-#     question = ["De quoi s’agit-il ?","Quelle est sa fréquence ?","Comment le reconnaître ?","Comment le diagnostic est-il posé ?","Que pouvez-vous faire ?","Que peut faire votre médecin?","En savoir plus ?"]
-#     resultat = []
-#     for gi in ggo :
-#         g=gi.text
-#         for element in question:
-#             if element != g :
-#                 resultat.append(g)
 
-#     print(resultat)
-
-#     driver.close()
-a = ["a","g","l","w"]
-c = "ayyyyfsgseelbbbwpp"
-def separateInfo(question,info)
-    for elements in question :
-        for g in info:
-            if elements == g:
-                info = info.replace(g,"[TITRE]")
-    rs=c.split("[TITRE]")
-    f= []
-    for j in range(len(question)):
-        info = {
-                question[j] : rs[j+1],
-            }
-        f.append(info)
+class ScrapInfoSante():
+    def __init__(self):
+        self.path = "/home/luca/Documents/Code/Selenium/chromedriver"
+        self.service = Service(self.path)
+        self.chop = webdriver.ChromeOptions()
+        self.chop.add_extension("tools/I-don-t-care-about-cookies.crx")
+        self.driver = webdriver.Chrome(service=self.service,options=self.chop)
         
-print(f)
+    def Get_result_search(self,query):
+        TextSearchResult= []
+        UrlSearchResult=[]
+        ListResult = []
+        self.driver.get(f"https://www.infosante.be/recherche?q={query}")
+        listeInfo=self.driver.find_elements(By.CLASS_NAME, 'morphsearch-content')
+        for itemInfo in listeInfo:
+            TextInfo = itemInfo.find_element(By.XPATH,'//*[@id="morphsearch"]/div/div/div/ul').text
+        TextSearchResult = TextInfo.split("\n")
+        for i in range(1,len(TextSearchResult)+1):
+            xpath=f'//*[@id="morphsearch"]/div/div/div/ul/li[{i}]/a'
+            geturl = self.driver.find_element(By.XPATH,xpath).get_attribute('href')
+            UrlSearchResult.append(geturl)
+        self.driver.close()
+        for j in range(len(TextSearchResult)):
+            info = {
+                'titre' : TextSearchResult[j],
+                'url': UrlSearchResult[j]
+            }
+            ListResult.append(info)
+        return ListResult
+
+    def GetInfoSante(self,URLSANTE):
+        self.driver.get(URLSANTE)
+        Getp=self.driver.find_elements(By.XPATH, '//*[@id="main"]/div/div/div[1]/div[3]')
+        OriginList=[]
+        for getp in Getp :
+            GetpText=getp.text
+            OriginList.append(GetpText)
+        self.driver.close()
+        FirstSplit= OriginList[0].split("\n")
+        question = ["De quoi s’agit-il ?","Quelle est sa fréquence ?","Comment le reconnaître ?","Comment le diagnostic est-il posé ?","Que pouvez-vous faire ?","Que peut faire votre médecin?","En savoir plus ?"]
+        NewQuestion=[]
+        for i in range(len(FirstSplit)-1):
+            if FirstSplit[i] in question:
+                NewQuestion.append(FirstSplit[i])
+                FirstSplit[i]="[Titre]"
+        Joined = "".join(FirstSplit)
+        Splited = Joined.split("[Titre]")
+        FinalResult=[]
+        for x in range (len(Splited)-1):
+            info = {
+                NewQuestion[x]:Splited[x+1],
+            }
+            FinalResult.append(info)
+        
+        return FinalResult
+Sc = ScrapInfoSante()
+print(Sc.GetInfoSante("file:///home/luca/Documents/Code/Selenium/Malaria%20(paludisme)%20%C2%B7%20Info%20sant%C3%A9.html"))
