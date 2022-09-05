15d361719b9840880acc69092a2681649ffc3f10
LucaZH <lucarandria1@gmail.com>
2022-09-05 10:06:26 +0300

    ds

--- app.py
+++ app.py
@@ -24,7 +24,11 @@ def main():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 recipient_id = messaging_event["sender"]["id"]
-                api.verifyuser(recipient_id)
+                # api.verifyuser(recipient_id)
+                try :
+                    api.insertuser
+                except:
+                    pass
                 Rimuru.send_action(recipient_id,"mark_seen")
                 user_state = api.getuserinfo(recipient_id,"state")
                 if messaging_event.get("message"):
