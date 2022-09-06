48afc32b086b8ce589820488fc57856455d8d3c5
LucaZH <lucarandria1@gmail.com>
2022-09-06 14:05:17 +0300

    again

--- tools/Bot/messenger.py
+++ tools/Bot/messenger.py
@@ -95,13 +95,13 @@ class Messenger:
                         {
                             "title":f"{info_fact['titre']}",
                             "image_url":"https://cdn-icons-png.flaticon.com/512/2764/2764545.png",
-                            "subtitle":f"{(info_fact['text'][:900] + '..') if len(info_fact['text']) > 900 else info_fact['text']}",
+                            "subtitle":f"{info_fact['text'][:100]}",
                             "buttons":[
                                 {
                                     "type":"postback",
                                     "title":"Lire",
                                     "payload":json.dumps({
-                                        'read': (info_fact['text'][:900] + '..') if len(info_fact['text']) > 900 else info_fact['text']
+                                        'read': info_fact['text'][:100]
                                     })
                                 }              
                             ]      
